2b18d8ac4a04928a8764c1f234575ede39c7e831
pierreozoux <pierre@ozoux.net>
2018-05-30 11:29:23 +0200

    Fix

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -eux
 
-#fio ./eval.fio --minimal --output-format=terse --output=results.csv
-fio ./eval.fio --minimal
+fio ./eval.fio --minimal --output-format=terse --output=results.csv
 fio2gnuplot -t rook-bw -b -g -p '*_bw*'
 fio2gnuplot -t rook-iops -i -g -p '*_iops*'
 python -m SimpleHTTPServer 8000
